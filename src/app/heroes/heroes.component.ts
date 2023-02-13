e8346b01f02c82ce1fe920eefdca7536c0a295a2
Isaac Brian <ISANT104@ford.com>
2023-02-13 11:41:51 -0300

    get by Hero implemented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../models/hero.model';
+import { HEROES } from '../models/mock.heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-hero: Hero = {
-  id: 1,
-  name:"Super Man"
-}
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: "Super Man"
   }
-
+  heroes = HEROES;
+ selectedHero?: Hero 
+ 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero
+  }
 }
+  
---
+++ src/app/models/mock.heroes.ts
@@ -0,0 +1,32 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: "Spider-Man"
+    },
+    {
+        id: 2,
+        name: "Wolverine"
+    },
+    {
+        id: 3,
+        name: "Super-Man"
+    },
+    {
+        id: 4,
+        name: "Wonder-Woman"
+    },
+    {
+        id: 5,
+        name: "Flash"
+    },
+    {
+        id: 6,
+        name: "Hulk"
+    },
+    {
+        id: 7,
+        name: "Iron-Man"
+    }
+]
